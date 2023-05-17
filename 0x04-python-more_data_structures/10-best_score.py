86a2761a89eb33f2a9fd091fab32af96a105e846
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-05-17 13:43:00 -0700

    mandatory tasks

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = None
+    for i in a_dictionary:
+        best = i if best is None else best
+        if a_dictionary[i] > a_dictinary[best]:
+            best = i
+
+     return best
