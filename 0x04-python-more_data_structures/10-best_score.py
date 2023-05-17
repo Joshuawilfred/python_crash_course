c57630ccab99ddbcc3c86bc20777cf67ec3e9cbb
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-05-17 14:05:04 -0700

    Roman to Integer: Elegance, Efficiency, Triumph!

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    best = None
-    for i in a_dictionary:
-        best = i if best is None else best
-        if a_dictionary[i] > a_dictinary[best]:
-            best = i
-
-     return best
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
