cd8ec7dee31673fcd11feda6fb848bec960238bd
Joshua Wilfred <joshuakaaya216@gmail.com>
2023-05-16 19:02:47 -0700

    progress | mandatory task

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def dividible_by_2(my_list=[]):
+    return [not (i % 2) for i in my_list]
---
+++ 0x03-python-data_structures/12-switch.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = 80
+b = 10
+a, b = b, a
+print("a={:} - b={:d}".format(a,b))
